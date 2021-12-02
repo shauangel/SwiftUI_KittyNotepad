e0b1d5a3349a51bc93683950dfbf1b6117f9efca
Angel Shau <shauangel2000@gmail.com>
2021-12-02 10:33:02 +0800

    Initial Commit

---
+++ SwiftUI_KittyNotepad/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_KittyNotepad
+//
+//  Created by 邵安祺 on 2021/12/2.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_KittyNotepad/SwiftUI_KittyNotepadApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_KittyNotepadApp.swift
+//  SwiftUI_KittyNotepad
+//
+//  Created by 邵安祺 on 2021/12/2.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_KittyNotepadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
